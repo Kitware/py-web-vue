4404de3e057c433ec241e1b081b531e922d59343
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-06-08 15:45:45 -0600

    doc(example): update 03_applications/vtk-file-loading

--- examples/03_applications/vtk-file-loading/app.py
+++ examples/03_applications/vtk-file-loading/app.py
@@ -14,15 +14,10 @@ app.layout = "./template.html"
 app.state = {
     "files": None,
     "meshes": [],
+    "field": "solid",
+    "fields": [],
 }
 app.vue_use = ["vuetify", "vtk"]
-
-# -----------------------------------------------------------------------------
-# VTK pipeline
-# -----------------------------------------------------------------------------
-
-reader = vtkXMLPolyDataReader()
-reader.ReadFromInputStringOn()
 
 # -----------------------------------------------------------------------------
 # Callbacks
@@ -30,18 +25,48 @@ reader.ReadFromInputStringOn()
 
 @app.change("files")
 def load_client_files():
-    files = app.get("files")
+    field = 'solid'
+    fields = {
+        'solid': { 'value':'solid', 'text': 'Solid color', 'range':[0, 1]},
+    }
     meshes = []
+    files = app.get("files")
+
     if files:
         for file in files:
-            base64Str = file.get('base64')
-            bytes = base64.b64decode(base64Str)
+            print(f'Load {file.get("name")}')
+            bytes = file.get('content')
+            reader = vtkXMLPolyDataReader()
+            reader.ReadFromInputStringOn()
             reader.SetInputString(bytes)
             reader.Update()
             ds = reader.GetOutputAsDataSet(0)
-            meshes.append(app.mesh(ds))
-        app.set('meshes', meshes)
+            point_arrays = []
+            pd = ds.GetPointData()
+            nb_arrays = pd.GetNumberOfArrays()
+            for i in range(nb_arrays):
+                array = pd.GetArray(i)
+                name = array.GetName()
+                min, max = array.GetRange(-1)
+                fields[name] = {"name": name, "range": [min, max], 'value': name, 'text': name, 'scalarMode': 3 }
+                point_arrays.append(name)
 
+            cell_arrays = []
+            cd = ds.GetCellData()
+            nb_arrays = cd.GetNumberOfArrays()
+            for i in range(nb_arrays):
+                array = cd.GetArray(i)
+                name = array.GetName()
+                min, max = array.GetRange(-1)
+                fields[name] = {"name": name, "range": [min, max], 'value': name, 'text': name, 'scalarMode': 4 }
+                cell_arrays.append(name)
+
+            meshes.append(app.mesh(ds, point_arrays=point_arrays, cell_arrays=cell_arrays))
+
+    app.set('field', field)
+    app.set('fields', fields)
+    app.set('meshes', meshes)
+    print(f'show {len(meshes)} meshes')
 
 # -----------------------------------------------------------------------------
 # CLI

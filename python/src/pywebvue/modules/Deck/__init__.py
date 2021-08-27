90d4d3a88a880403e1564e151764b88098197132
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-08-27 17:45:52 -0600

    fix(Deck): Add module to support Deck.gl

---
+++ python/src/pywebvue/modules/Deck/__init__.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+serve_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "serve"))
+
+serve = {"__vue_deck": serve_path}
+scripts = ["/__vue_deck/vue-deck.umd.min.js"]
+vue_use = ['VueDeck']
+
+
+def to_jsonInput(deck):
+    return json.loads(deck.to_json())

ee5a735757cb51568f60b6016af8273d82281c7d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-08-27 17:46:44 -0600

    doc(Mapping): Add mapping demo

---
+++ examples/05_streamlit/MappingDemo/app.py
@@ -0,0 +1,138 @@
+import os
+import pandas as pd
+import pydeck as pdk
+
+from urllib.error import URLError
+
+from pywebvue import App
+from pywebvue.modules import Deck
+
+# -----------------------------------------------------------------------------
+# Getting a Mapbox API key
+# -----------------------------------------------------------------------------
+# By default, pydeck 0.6 provides basemap tiles through Carto.
+#
+# You can optionally use a Mapbox API key, by registering for Mapbox via
+# this link [1]. You should then create a new public API token [2].
+# You can learn more about Mapbox tokens via their documentation [3].
+#
+# [1] https://account.mapbox.com/auth/signup/
+# [2] https://account.mapbox.com/access-tokens/
+# [3] https://docs.mapbox.com/help/how-mapbox-works/access-tokens/#how-access-tokens-work
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# App Setup
+# -----------------------------------------------------------------------------
+
+app = App("NYC Uber Ridesharing Data")
+app.state = {
+    "layerNames": [
+        "Bike Rentals",
+        "Bart Stop Exits",
+        "Bart Stop Names",
+        "Outbound Flow",
+    ],
+    "activeLayers": [
+        "Bike Rentals",
+        "Bart Stop Exits",
+        "Bart Stop Names",
+        "Outbound Flow",
+    ],
+    "deck": None,
+    "error": None,
+    "mapboxApiKey": os.environ[
+        "MAPBOX_API_KEY"
+    ],  # <-- Expect MAPBOX_API_KEY environment variable
+}
+
+# -----------------------------------------------------------------------------
+
+app.enableModule(Deck)
+
+# -----------------------------------------------------------------------------
+
+
+def from_data_file(filename):
+    url = (
+        "https://raw.githubusercontent.com/streamlit/"
+        "example-data/master/hello/v1/%s" % filename
+    )
+    return pd.read_json(url)
+
+
+ALL_LAYERS = {
+    "Bike Rentals": pdk.Layer(
+        "HexagonLayer",
+        data=from_data_file("bike_rental_stats.json"),
+        get_position=["lon", "lat"],
+        radius=200,
+        elevation_scale=4,
+        elevation_range=[0, 1000],
+        extruded=True,
+    ),
+    "Bart Stop Exits": pdk.Layer(
+        "ScatterplotLayer",
+        data=from_data_file("bart_stop_stats.json"),
+        get_position=["lon", "lat"],
+        get_color=[200, 30, 0, 160],
+        get_radius="[exits]",
+        radius_scale=0.05,
+    ),
+    "Bart Stop Names": pdk.Layer(
+        "TextLayer",
+        data=from_data_file("bart_stop_stats.json"),
+        get_position=["lon", "lat"],
+        get_text="name",
+        get_color=[0, 0, 0, 200],
+        get_size=15,
+        get_alignment_baseline="'bottom'",
+    ),
+    "Outbound Flow": pdk.Layer(
+        "ArcLayer",
+        data=from_data_file("bart_path_stats.json"),
+        get_source_position=["lon", "lat"],
+        get_target_position=["lon2", "lat2"],
+        get_source_color=[200, 30, 0, 160],
+        get_target_color=[200, 30, 0, 160],
+        auto_highlight=True,
+        width_scale=0.0001,
+        get_width="outbound",
+        width_min_pixels=3,
+        width_max_pixels=30,
+    ),
+}
+
+
+@app.change("activeLayers")
+def update_map():
+    selected_layers = [
+        layer
+        for layer_name, layer in ALL_LAYERS.items()
+        if layer_name in app.state.get("activeLayers")
+    ]
+
+    if selected_layers:
+        deck = pdk.Deck(
+            map_provider="mapbox",
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": 37.76,
+                "longitude": -122.4,
+                "zoom": 11,
+                "pitch": 50,
+            },
+            layers=selected_layers,
+        )
+        app.set("deck", Deck.to_jsonInput(deck))
+    else:
+        app.set("error", "Please choose at least one layer above.")
+
+
+# -----------------------------------------------------------------------------
+# Start server
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    app.on_ready = update_map
+    app.run_server()
---
+++ examples/05_streamlit/uber-nyc-pickups/app.py
@@ -0,0 +1,160 @@
+"""An example of showing geographic data."""
+
+import os
+import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
+
+from pywebvue import App
+from pywebvue.modules import VueVegaEmbed, Deck
+
+# -----------------------------------------------------------------------------
+# App Setup
+# -----------------------------------------------------------------------------
+
+app = App("NYC Uber Ridesharing Data", debug=True)
+app.state = {
+    "hourBreackDown": None,
+    "pickupHour": 0,
+    "chartTitle": "",
+    "mapboxApiKey": os.environ[
+        "MAPBOX_API_KEY"
+    ],  # <-- Expect MAPBOX_API_KEY environment variable
+}
+
+app.enableModule(VueVegaEmbed)
+app.enableModule(Deck)
+
+# -----------------------------------------------------------------------------
+
+
+# LOADING DATA
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+
+data = load_data(100000)
+
+# CREATING FUNCTION FOR MAPS
+
+map_list = [
+    {
+        "id": "nyc",
+        "title": "All New York City",
+        "lat": np.average(data["lat"]),
+        "lon": np.average(data["lon"]),
+        "zoom": 11,
+    },
+    {
+        "id": "lga",
+        "title": "La Guardia Airport",
+        "lat": 40.7900,
+        "lon": -73.8700,
+        "zoom": 12,
+    },
+    {
+        "id": "jfk",
+        "title": "JFK Airport",
+        "lat": 40.6650,
+        "lon": -73.7821,
+        "zoom": 11,
+    },
+    {
+        "id": "nwk",
+        "title": "Newark Airport",
+        "lat": 40.7090,
+        "lon": -74.1805,
+        "zoom": 11,
+    },
+]
+
+
+def map(data, lat, lon, zoom, **kwarg):
+    deck = pdk.Deck(
+        map_provider="mapbox",
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": lat,
+            "longitude": lon,
+            "zoom": zoom,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                radius=100,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+        ],
+    )
+    app.set(kwarg["id"], Deck.to_jsonInput(deck))
+    app.set(f'{kwarg["id"]}Title', kwarg["title"])
+
+
+@app.change("pickupHour")
+def updateData():
+    hour_selected = app.get("pickupHour")
+    app.set(
+        "chartTitle",
+        f"All New York City from {hour_selected}:00 and {hour_selected + 1}:00",
+    )
+
+    # FILTERING DATA BY HOUR SELECTED
+    filtered_data = data[data[DATE_TIME].dt.hour == hour_selected]
+
+    for item in map_list:
+        map(filtered_data, **item)
+
+    # FILTERING DATA FOR THE HISTOGRAM
+    filtered = filtered_data[
+        (data[DATE_TIME].dt.hour >= hour_selected)
+        & (data[DATE_TIME].dt.hour < (hour_selected + 1))
+    ]
+
+    hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
+
+    chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
+
+    # LAYING OUT THE HISTOGRAM SECTION
+    app.set(
+        "hourBreackDown",
+        VueVegaEmbed.altair_to_spec(
+            alt.Chart(chart_data)
+            .mark_area(
+                interpolate="step-after",
+            )
+            .properties(width=600, height=150)
+            .encode(
+                x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+                y=alt.Y("pickups:Q"),
+                tooltip=["minute", "pickups"],
+            )
+            .configure_mark(opacity=0.5, color="red")
+        ),
+    )
+
+
+updateData()
+
+# -----------------------------------------------------------------------------
+# Start server
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    app.run_server()

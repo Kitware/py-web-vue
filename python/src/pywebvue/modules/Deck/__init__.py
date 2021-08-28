e71ab160fcbfe6bafede147ce8b7e0db4ae4752e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-08-27 18:10:55 -0600

    style(black): Reformat python

--- python/src/pywebvue/modules/Deck/__init__.py
+++ python/src/pywebvue/modules/Deck/__init__.py
@@ -5,7 +5,7 @@ serve_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "serve"))
 
 serve = {"__vue_deck": serve_path}
 scripts = ["/__vue_deck/vue-deck.umd.min.js"]
-vue_use = ['VueDeck']
+vue_use = ["VueDeck"]
 
 
 def to_jsonInput(deck):

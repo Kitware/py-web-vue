c6104926ab2d4b67d1c8d78902057263ea074398
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-08-31 14:30:46 -0600

    style(AGGrid): Fix module code formatting

--- python/src/pywebvue/modules/AGGrid/__init__.py
+++ python/src/pywebvue/modules/AGGrid/__init__.py
@@ -21,46 +21,55 @@ vue_use = ["AgGrid"]
 
 type_mapper = {
     "b": ["textColumn"],
-    "i": [], # ["numericColumn", "numberColumnFilter"],
-    "u": [], # ["numericColumn", "numberColumnFilter"],
-    "f": [], # ["numericColumn", "numberColumnFilter"],
+    "i": [],  # ["numericColumn", "numberColumnFilter"],
+    "u": [],  # ["numericColumn", "numberColumnFilter"],
+    "f": [],  # ["numericColumn", "numberColumnFilter"],
     "c": [],
-    "m": [], # ['timedeltaFormat'],
-    "M": [], # ["dateColumnFilter", "shortDateTimeFormat"],
+    "m": [],  # ['timedeltaFormat'],
+    "M": [],  # ["dateColumnFilter", "shortDateTimeFormat"],
     "O": [],
     "S": [],
     "U": [],
     "V": [],
 }
 
+
 def cast_to_serializable(value):
-    isoformat = getattr(value, 'isoformat', None)
-    if ((isoformat) and callable(isoformat)):
+    isoformat = getattr(value, "isoformat", None)
+    if (isoformat) and callable(isoformat):
         return isoformat()
     elif isinstance(value, Number):
-        if (np.isnan(value) or np.isinf(value)):
+        if np.isnan(value) or np.isinf(value):
             return value.__str__()
         return value
 
     return value.__str__()
 
+
 # -----------------------------------------------------------------------------
 
-def to_grid(dataframe, sortable=[], filter=[], rowGroup=[], resizable=[], headerNames={}):
+
+def to_grid(
+    dataframe, sortable=[], filter=[], rowGroup=[], resizable=[], headerNames={}
+):
     # Setup columns definitions
     columnDefs = []
     for col_name, col_type in zip(dataframe.columns, dataframe.dtypes):
-        columnDefs.append({
-            'headerName': headerNames[col_name] if col_name in headerNames else col_name,
-            'field': col_name,
-            'type': type_mapper.get(col_type.kind, []),
-            'sortable': col_name in sortable,
-            'filter': col_name in filter,
-            # 'rowGroup': col_name in rowGroup,
-            'resizable': col_name in resizable,
-            # "minWidth": min_column_width,
-            # "editable": editable,
-        })
+        columnDefs.append(
+            {
+                "headerName": headerNames[col_name]
+                if col_name in headerNames
+                else col_name,
+                "field": col_name,
+                "type": type_mapper.get(col_type.kind, []),
+                "sortable": col_name in sortable,
+                "filter": col_name in filter,
+                # 'rowGroup': col_name in rowGroup,
+                "resizable": col_name in resizable,
+                # "minWidth": min_column_width,
+                # "editable": editable,
+            }
+        )
 
     # Extract row data
     rowData = dataframe.applymap(cast_to_serializable).to_dict(orient="records")
@@ -70,13 +79,15 @@ def to_grid(dataframe, sortable=[], filter=[], rowGroup=[], resizable=[], header
         "rowData": rowData,
     }
 
+
 # -----------------------------------------------------------------------------
+
 
 def update_columnDefs(grid, change_set={}):
     list_to_update = grid
-    if 'columnDefs' in grid:
-        list_to_update = grid['columnDefs']
+    if "columnDefs" in grid:
+        list_to_update = grid["columnDefs"]
 
     for item in list_to_update:
-        if item['field'] in change_set:
-            item.update(change_set[item['field']])
+        if item["field"] in change_set:
+            item.update(change_set[item["field"]])

83107e9ec459ab97bab5c0923938239dcb6a52ff
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-09-03 12:01:17 -0600

    style(comment): Remove commented code

--- python/src/pywebvue/modules/Markdown/__init__.py
+++ python/src/pywebvue/modules/Markdown/__init__.py
@@ -6,4 +6,3 @@ serve_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "serve"))
 serve = {"__markdown_it": serve_path}
 scripts = ["/__markdown_it/markdown-it-vue.umd.min.js"]
 styles = ["/__markdown_it/markdown-it-vue.css"]
-# vue_use = ["VueMarkdown"]

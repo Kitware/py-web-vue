435c7b29e394a4099a8f4a04a9983db6ce353646
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-09-03 11:55:09 -0600

    fix(Markdown): Add markdown module

---
+++ examples/PlainPython/Markdown/Simple/app.py
@@ -0,0 +1,27 @@
+import os
+from pywebvue import App
+from pywebvue.modules import Markdown
+
+app = App("Basic Markdown Example")
+app.enableModule(Markdown)
+
+# Read markdown file
+md_file_path = os.path.join(os.path.dirname(__file__), "demo.md")
+with open(md_file_path) as md:
+    markdown_content = md.read()
+
+app.state = {"mdContent": markdown_content}
+app.layout = """
+    <markdown-it-vue
+        class="md-body"
+        style="padding: 20px"
+        :content="mdContent"
+    />
+"""
+
+# -----------------------------------------------------------------------------
+# Start server
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    app.run_server()
---
+++ python/src/pywebvue/modules/Markdown/__init__.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+serve_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "serve"))
+
+serve = {"__markdown_it": serve_path}
+scripts = ["/__markdown_it/markdown-it-vue.umd.min.js"]
+styles = ["/__markdown_it/markdown-it-vue.css"]
+# vue_use = ["VueMarkdown"]

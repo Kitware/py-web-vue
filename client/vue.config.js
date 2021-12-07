dd842ffd94ab96ed1b21b20388af66ee55575f0b
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2021-12-07 13:10:59 -0700

    fix(client): smaller bundle size

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const vtkChainWebpack = require('vtk.js/Utilities/config/chainWebpack');
 
 module.exports = {
+  productionSourceMap: false,
   runtimeCompiler: true,
   outputDir: path.resolve(__dirname, '../python/src/pywebvue/www'),
   chainWebpack: (config) => {

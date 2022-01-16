be0b9d90a7dfd6cdb0a790a7fff1f1b21227320e
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-01-15 19:21:24 -0700

    fix(plotly): add event helper

--- vue-components/vue-plotly/src/main.js
+++ vue-components/vue-plotly/src/main.js
@@ -1,7 +1,37 @@
-import { Plotly } from 'vue-plotly'
+import { Plotly } from 'vue-plotly';
+
+function safeObject(item) {
+  const newItem = {};
+  const keys = Object.keys(item);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (key === 'data') {
+      continue;
+    }
+    const value = item[key];
+    if (Array.isArray(value)) {
+      newItem[key] = value.map(safeObject);
+    } else {
+      try {
+        JSON.stringify(value);
+        newItem[key] = value;
+      } catch (e) {
+        continue;
+      }
+    }
+  }
+  return newItem;
+}
+
+function safeEvent(event) {
+  const safe = safeObject(event);
+  safe.data = event?.points[0]?.data;
+  return safe;
+}
 
 export default {
   install(Vue) {
-    Vue.component("Plotly", Plotly);
+    Vue.component('Plotly', Plotly);
+    window.VuePlotly.safe = safeEvent;
   },
 };
--- vue-components/vue-plotly/vue.config.js
+++ vue-components/vue-plotly/vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const MODULES = "../../python/src/pywebvue/modules"
-const Deck = "Plotly/serve"
+const MODULES = '../../python/src/pywebvue/modules';
+const Deck = 'Plotly/serve';
 
 module.exports = {
   outputDir: path.resolve(__dirname, MODULES, Deck),
